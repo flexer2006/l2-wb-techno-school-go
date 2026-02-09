0f0230723c08db9c11b6acd2e8026d8c7a038275
andrew <andrewgo1133official@gmail.com>
2026-02-09 16:20:48 +0300

    8

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/flexer2006/l2-wb-techno-school-go
 
 go 1.25.5
+
+require github.com/beevik/ntp v1.5.0
+
+require (
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+)
--- l/eight/eight.go
+++ l/eight/eight.go
@@ -1,1 +1,34 @@
 package eight
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+func eight() {
+	var (
+		server  string
+		timeout time.Duration
+	)
+
+	flag.StringVar(&server, "server", "pool.ntp.org", "NTP server to query (default: pool.ntp.org)")
+	flag.DurationVar(&timeout, "timeout", 5*time.Second, "NTP query timeout")
+	flag.Parse()
+
+	resp, err := ntp.QueryWithOptions(server, ntp.QueryOptions{Timeout: timeout})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if err := resp.Validate(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	t := time.Now().Add(resp.ClockOffset).UTC()
+	fmt.Println(t.Format(time.RFC3339Nano))
+}
