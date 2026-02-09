033885fb13e8ca49881da94b4f3910fcbfce73b7
andrew <andrewgo1133official@gmail.com>
2026-02-09 16:34:06 +0300

    11

--- l/eleven/eleven.go
+++ l/eleven/eleven.go
@@ -1,1 +1,44 @@
 package eleven
+
+import (
+	"slices"
+	"sort"
+	"strings"
+)
+
+func FindAnagrams(words []string) map[string][]string {
+	type group struct {
+		first string
+		words []string
+	}
+
+	sigToGroup := make(map[string]*group, len(words))
+	for _, w := range words {
+		lw := strings.ToLower(w)
+		sig := sortedRunes(lw)
+		if g, ok := sigToGroup[sig]; ok {
+			g.words = append(g.words, lw)
+		} else {
+			sigToGroup[sig] = &group{
+				first: lw,
+				words: []string{lw},
+			}
+		}
+	}
+
+	result := make(map[string][]string, len(sigToGroup))
+	for _, g := range sigToGroup {
+		if len(g.words) <= 1 {
+			continue
+		}
+		sort.Strings(g.words)
+		result[g.first] = g.words
+	}
+	return result
+}
+
+func sortedRunes(s string) string {
+	r := []rune(s)
+	slices.Sort(r) // вместо sort.Slice(r, func(i, j int) bool { return r[i] < r[j] })
+	return string(r)
+}
